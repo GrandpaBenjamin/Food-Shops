7e9cb9ef8b1fe31e5fe63e2286057bf4608424dd
GrandpaBenjamin <59100390+GrandpaBenjamin@users.noreply.github.com>
2020-02-27 15:57:26 +0000

    Subway
    BETA - subway upload can choice what want. limited selections of meat and salad. to be continued

---
+++ Subway.py
@@ -0,0 +1,51 @@
+#subway chicken tendies
+
+from time import sleep
+from random import randint
+
+breads = ['9-Grain Wheat','Italian','Hearty Italian','Flatbread']
+salads = ['salad kind']
+meats = ['meat']
+premade_sandwiches = []
+
+def in_Stock(to_check,compare_with):
+    for x in compare_with:
+        if x.lower() == to_check.lower():
+            return True
+        elif to_check.lower == 'none':
+            return True
+    return False
+
+def welcome():
+    print('hello welcome to subway')
+    while True:
+        bread = input('What bread would you like? ')
+        if in_Stock(bread,breads):
+            break
+        elif not(in_Stock(bread,breads)):
+            print('That bread is not in stock')
+            continue
+        
+    print('ok')
+    while True:
+        meat = input('What meat would you like? ')
+        if in_Stock(meat,meats):
+            break
+        elif not(in_Stock(meat,meats)):
+            print('We dont have that in stock')
+            continue
+    print('ok')
+    while True:
+        salad = input('What salad would you like')
+        if in_Stock(salad,salads):
+            break
+        elif not(in_Stock(salad,salads)):
+            print('That salad is not in stock')
+            continue
+    print('ok')
+
+def payment(bread,meat,salad):
+    pass #make here compiles choices
+
+
+welcome()
